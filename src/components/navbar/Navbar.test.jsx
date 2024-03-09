bae52828ee6adf6d18e3eb4c39796c733de9030d
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2024-03-09 16:52:10 +0100

    Set up vitest to test react components
    Write a simple test for the Navbar component

---
+++ src/components/navbar/Navbar.test.jsx
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react"
+import NavBar from "./Navbar.jsx";
+import { BrowserRouter } from 'react-router-dom'
+
+describe('Navbar component', () => {
+
+    it('renders home link', () => {
+        render(<BrowserRouter><NavBar /></BrowserRouter>);
+        expect(screen.getAllByRole('link')[0].textContent).toMatch(/home/i);
+    });
+    
+    it('renders shop link', () => {
+        render(<BrowserRouter><NavBar /></BrowserRouter>);
+        expect(screen.getAllByRole('link')[1].textContent).toBe('Shop');
+    })
+})
