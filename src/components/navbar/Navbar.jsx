80934204131afc0445b92f3dd43373517368f33f
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2024-03-23 20:29:52 +0100

    Add cart widget to the NavBar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import styles from './Navbar.module.css'
+import cartImg from '../../assets/shoppingcart.png'
+import PropTypes from 'prop-types'
 
 export default function NavBar ( { cart } ) {
 
@@ -7,6 +9,7 @@ export default function NavBar ( { cart } ) {
 
     return (
         <header className={styles.navbar}>
+            <div>Logo</div>
             <nav>
                 {name === undefined ? <div className={styles.highlight}><Link to="/">Home</Link></div> : <div className={styles.linkHolder}><Link to="/">Home</Link></div>}
                 {name === "menswear" ? <div className={styles.highlight}><Link to="/menswear">Menswear</Link></div> : <div className={styles.linkHolder}><Link to="/menswear">Menswear</Link></div>}
@@ -14,7 +17,21 @@ export default function NavBar ( { cart } ) {
                 {name === "jewelery" ? <div className={styles.highlight}><Link to="/jewelery">Jewelery</Link></div> : <div className={styles.linkHolder}><Link to="/jewelery">Jewelery</Link></div>}
                 {name === "electronics" ? <div className={styles.highlight}><Link to="/electronics">Electronics</Link></div> : <div className={styles.linkHolder}><Link to="/electronics">Electronics</Link></div>}
             </nav>
-            <div> { cart.length }</div>
+            <Link to="cart">
+                <div className={styles.cartIcon}>
+                    <img src={cartImg} alt="" />
+                    { cart.length > 0 &&
+                        <div className={styles.itemsInCart}>
+                            <span>{cart.length}</span>
+                        </div>
+                    }
+
+                </div>
+            </Link>
         </header>
     )
+}
+
+NavBar.propTypes = {
+    cart: PropTypes.array
 }
